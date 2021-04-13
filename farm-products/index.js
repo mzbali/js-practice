5cb363051118ee7b91cb2513cea9ceb96ce1cd5a
mzbali <mahmud.bali@tuta.io>
2021-04-14 03:23:56 +0600

    added flash messages for farm

--- farm-products/index.js
+++ farm-products/index.js
@@ -4,14 +4,23 @@ const Product = require('./models/product');
 const Farm = require('./models/farm');
 const path = require('path');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const app = express();
+const sessionConfig = { secret: 'secret', resave: false, saveUninitialized: false }
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(session(sessionConfig))
+app.use(flash()); //adds flash method to req object
+app.use((req, res, next) => {
+    res.locals.messages = req.flash('messages');
+    next();
+})
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
@@ -43,6 +52,7 @@ app.get('/farms/:id', async (req, res) => {
 app.post('/farms', async (req, res) => {
     const farm = new Farm(req.body);
     await farm.save();
+    req.flash('messages', 'Succesfully saved new farm :)');
     res.redirect('/farms');
 })
 
@@ -69,8 +79,9 @@ app.post('/farms/:id/products', async (req, res) => {
 app.delete('/farms/:id', async (req, res) => {
     const { id } = req.params;
     const farm = await Farm.findByIdAndDelete(id);
+    req.flash('messages', 'Succesfully deleted farm :||');
     res.redirect(`/farms`);
-})
+});
 
 
 
@@ -84,17 +95,17 @@ app.get('/products', async (req, res) => {
         const products = await Product.find({ category });
         res.render('products/index', { products, category });
     }
-})
+});
 
 app.get('/products/new', (req, res) => {
     res.render('products/new', { categories });
-})
+});
 
 app.post('/products', async (req, res) => {
     const product = new Product(req.body);
     await product.save();
     res.redirect('/products');
-})
+});
 
 app.get('/products/:id', async (req, res, next) => {
     try {
@@ -105,30 +116,30 @@ app.get('/products/:id', async (req, res, next) => {
     } catch (e) {
         next(e);
     }
-})
+});
 
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/edit', { product, categories });
-})
+});
 
 app.put('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
     res.redirect(`/products/${id}`);
-})
+});
 
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
     await Product.findByIdAndDelete(id);
     res.redirect('/products');
-})
+});
 
 app.use((err, req, res, next) => {
     res.send("Error products not found")
-})
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
-})
+});
